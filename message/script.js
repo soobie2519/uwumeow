658eb80a2b900070162f8328dca49380e53b5a5c
Tino Nonnast <134141296+Tinonnast@users.noreply.github.com>
2026-03-12 19:24:06 +0100

    Add files via upload

---
+++ message/script.js
@@ -0,0 +1,18 @@
+function submit_button() {
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+    console.log("email:", email, "message:", message); // Check these exist
+
+    fetch("https://discord.com/api/webhooks/1481383598044086302/BRulxsCALTaXTD0gLr1S3B5LbZXyMv8Yg66uEGZr0a2si6oAAWsKpKALo5womG30N9XR", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: message || "no message",
+            username: email || "Unknown"
+        })
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))  // Shows Discord's exact error
+        .catch(err => console.error(err));
+
+}
